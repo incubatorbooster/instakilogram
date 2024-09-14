b569dc64cc19896934911a5716acbcbcb6e5697c
apelsinom <apelsinom@mail.ru>
2024-09-14 13:56:54 +0300

    feat: installed storybook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import "@/shared/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/shared/styles/globals.css'
+import '@fontsource/inter'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/400-italic.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
